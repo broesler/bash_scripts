0d1ffccddf8a3cec0f82310b39a80368863384b1
Bernard Roesler <bernard.roesler@gmail.com>
2016-12-12 15:38:25 -0500

    Move sshx.sh here.

---
+++ sshx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ssh to babylon x
+# Check if we're already in ssh session
+if [ -z "$SSH_TTY" ]; then
+    # connecting from plain terminal
+    command ssh -X d26725q@babylon$1.thayer.dartmouth.edu
+else
+    read -p "Warning: Aready in SSH session. Ok to nest? [yn] >" > choice
+    case $choice in
+        y)
+            # connecting from plain terminal
+            command ssh -X d26725q@babylon$1.thayer.dartmouth.edu
+            ;;
+        *)
+            return 0
+    esac
+fi
