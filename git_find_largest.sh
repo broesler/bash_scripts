2eeb9d974b8fed17ec1d0146f9848dc431c8612a
Bernard Roesler <bernard.roesler@gmail.com>
2017-01-13 12:41:04 -0500

    Add clean exit to git_find_largest.

--- git_find_largest.sh
+++ git_find_largest.sh
@@ -8,11 +8,23 @@
 #       Author: Antony Stubbs
 #
 #===============================================================================
+
+# Take number of files to process
+if [ $# -eq 1 ]; then
+    n=$1
+else
+    n=10
+fi
+
 # set the internal field spereator to line break, so that we can iterate easily over the verify-pack output
 IFS=$'\n';
 
 # list all objects including their size, sort by size, take top 10
-objects=`git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head`
+objects=$(git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head --lines="$n")
+
+if [[ "$objects" == ".git/objects/pack/pack-*.pack: bad" ]]; then
+    exit 1
+fi
 
 echo "All sizes are in kB. The pack column is the size of the object, compressed, inside the pack file."
 
@@ -31,6 +43,6 @@ do
 	output="${output}\n${size},${compressedSize},${other}"
 done
 
-echo -e $output | column -t -s ', '
+echo -e $output | column -t -s ','
 #===============================================================================
 #===============================================================================
