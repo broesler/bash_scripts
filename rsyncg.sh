4e394ec4d958add48b3c64b5dfbf8aa5e32ebbca
Bernard Roesler <bernard.roesler@gmail.com>
2018-02-15 12:13:36 -0500

    Update rsync to use array for gitig. usage as one command.

--- rsyncg.sh
+++ rsyncg.sh
@@ -10,8 +10,8 @@
 
 usage ()
 {
-    echo "Usage: $0 [OPTION] SRC [SRC] DEST"    1>&2
-    echo "  See 'man rsync' for more options."  1>&2
+    printf "%s\n" "Usage: $(basename $0) [OPTION] SRC [SRC] DEST"\
+                  "   See 'man rsync' for more options." 1>&2
     exit 1
 }
 
@@ -19,15 +19,28 @@ if [ "$#" -lt 2 ]; then
     usage
 fi
 
-# If a local .gitignore file exists, 
-# else use the global, 
+# If a local and/or global .gitignore file exists, use it
 # else just rsync all files
-gitig=''
-if [ -r '.gitignore' ]; then
-    gitig="--include-from=.gitignore --exclude='*'"
-elif [ -r "$HOME/.gitignore_global" ]; then
-    echo "Warning! $0 is using .gitignore_global" 1>&2
-    gitig="--include-from=$HOME/.gitignore_global --exclude='*'"
+# TODO find all .gitignores in the repo:
+#   1. Get toplevel directory
+#   2. find "$toplevel" -name '.gitignore'
+#   Possible issue: how does .gitignore respect paths?
+#   Fix: Force this script to run from the top-level directory, but how to
+#   determine which argument is the "local" directory?
+gitig=()
+if [ -r .gitignore ]; then
+    gitig+=('--include-from=.gitignore')
+fi
+
+if [ -r "$HOME/.gitignore_global" ]; then
+    gitig+=("--include-from=$HOME/.gitignore_global")
+fi
+
+# If we're using a gitignore, exclude all the other files in the directory
+# NOTE the '*' does not get expanded by the double quotes below. A second set of
+# double quotes would perform the next expansion
+if [ -n "$gitig" ]; then
+    gitig+=('--exclude=*')
 fi
 
 # NOTE the order of "exclude"s and "include"s DOES matter!! See 'man rsync'.
@@ -39,7 +52,7 @@ rsync -crlmtvz --stats     \
     --exclude='*Icon*'     \
     --exclude='.git/'      \
     --include='*/'         \
-    $gitig                 \
+    "${gitig[@]}"          \
     "$@"
 
 #===============================================================================
