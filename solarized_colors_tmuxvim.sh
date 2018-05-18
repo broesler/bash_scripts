f186de3ba34b4324fb7eddb6f1bea6bdc492abb5
Bernard Roesler <bernard.roesler@gmail.com>
2018-05-17 22:44:59 -0400

    Clean up if statement. Use separate vim file.

--- solarized_colors_tmuxvim.sh
+++ solarized_colors_tmuxvim.sh
@@ -20,20 +20,24 @@ fi
 # BSD sed requires "-i ''" (space after -i)
 # GNU sed DOES NOT take space after -i ("-i''")
 if [[ "$1" == 'dark' ||  "$1" == 'light' ]]; then
-    # Set background to dark, using non-degraded 16-color mode
-    sed -i'' --follow-symlinks "/@colors-solarized/ s/'[a-z0-9]\+'/'$1'/" $HOME/.tmux.conf
-    sed -i'' --follow-symlinks "/set background=/ s/=[a-z]\+/=$1/" $HOME/.vimrc
-    sed -i'' --follow-symlinks "/let g:solarized_termcolors =/ s/= [0-9]\+/= 16/" $HOME/.vimrc
+    # Set background to (dark|light), using non-degraded 16-color mode
+    tmux_cval=$1
+    vim_cval=$1
+    ncol=16
 elif [[ "$1" == '256' ]]; then
     # Set background to dark, using degraded 256-color mode
-    sed -i'' --follow-symlinks "/@colors-solarized/ s/'[a-z0-9]\+'/'256'/" $HOME/.tmux.conf
-    sed -i'' --follow-symlinks "/set background=/ s/=[a-z0-9]\+/=dark/" $HOME/.vimrc
-    sed -i'' --follow-symlinks "/let g:solarized_termcolors =/ s/= [0-9]\+/= 256/" $HOME/.vimrc
+    tmux_cval=256
+    vim_cval=dark
+    ncol=256
 else
     usage
 fi
 
 # Apply the changes
+sed -i'' --follow-symlinks "/@colors-solarized/ s/'[a-z0-9]\+'/'${tmux_cval}'/" $HOME/.tmux.conf
+sed -i'' --follow-symlinks "/set background=/ s/=[a-z]\+/=${vim_cval}/" $HOME/.vimrc_solarized
+sed -i'' --follow-symlinks "/let g:solarized_termcolors =/ s/= [0-9]\+/= ${ncol}/" $HOME/.vimrc_solarized
+
 if [ -n "$TMUX" ]; then
     tmux source-file $HOME/.tmux.conf
 fi
