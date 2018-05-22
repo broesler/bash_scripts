e21a6c1113c9a1d065aa5596f11c1d3a57f3c234
Bernard Roesler <bernard.roesler@gmail.com>
2018-05-21 20:43:14 -0400

    Allow 2nd argument for end pattern.

--- ptb.sh
+++ ptb.sh
@@ -9,28 +9,44 @@
 #===============================================================================
 me=$(basename $0)
 
+usage() {
+    echo "Usage: $me [TMUX PANE] [ENDPAT]" 2>&1
+}
+
 if [ -z "$TMUX" ]; then
-  echo "Usage: $me must be run inside tmux session." 2>&1
+  echo "$me must be run inside tmux session!" 2>&1
+  usage
   exit 1
 fi
 
 if [ "$#" -eq 0 ]; then
-    echo "Usage: $me requires the tmux pane as an argument." 2>&1
+    echo "$me requires the tmux pane as an argument!" 2>&1
+    usage
     exit 1
 fi
 
 # Take first argument as pane
 tmuxswp="$1"
 
+if [ "$#" -eq 2 ]; then
+    endpat="$2"
+else
+    endpat='\[.*\]\$' # bash prompt
+fi
+
 # Copy ipython pane to current pane's stdout for parsing
 # sed steps:
 #   - On "Traceback" lines replace hold space with pattern space
 #   - On non-empty lines, append to hold space
 #   - When end of file reached, exchange hold and pattern space, print
-#   - delete all other lines
+#   - all other lines, delete pattern space (current line only)
 # Finally, remove last line, leaving just the Traceback message!
 tpane=$(tmux capture-pane -t "$tmuxswp" -p)
-echo "$tpane" | sed '/Traceback/h;//!H;${x;p};d' | sed '$d'
+if [ -z "$endpat" ]; then
+    echo "$tpane" | sed '/Traceback/h;//!H;${x;p};d' | sed '$d'
+else
+    echo "$tpane" | sed "/Traceback/{:1;/${endpat}/!{N;b1};h};\${x;p};d" | sed '$d'
+fi
 
 exit 0
 #===============================================================================
